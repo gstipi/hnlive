df62a4b1769756d7f590b6aae3190ec26d05d19f
Gregor Stipicic <gregor.stipicic@gmail.com>
2020-05-26 11:21:28 +0200

    fiddling with CSS and layout

--- lib/hnlive_web/live/page_live.ex
+++ lib/hnlive_web/live/page_live.ex
@@ -16,7 +16,6 @@ defmodule HNLiveWeb.PageLive do
   def render(assigns) do
     ~L"""
     <center>
-    <%= if length(@top_newest) > 0 do %>
     <table id="hnmain" border="0" cellpadding="0" cellspacing="0" width="85%" bgcolor="#f6f6ef">
       <tbody>
       <tr>
@@ -25,34 +24,55 @@ defmodule HNLiveWeb.PageLive do
             <tr>
               <td style="width:18px;padding-right:4px"></td>
               <td style="line-height:12pt; height:10px;">
-               <span class="pagetop"><b class="hnname">Hacker News Live</b></span>
+               <span class="pagetop"><b class="hnname">HN Top 10 Newest Posts Live</b></span>
               </td>
             </tr>
           </table>
         </td>
       </tr>
-    <tr id="pagespace" title="" style="height:10px">
-    </tr>
-    <tr>
-    <td>
+      <tr id="pagespace" title="" style="height:10px"></tr>
+      <tr>
+      <td>
+    <%= if length(@top_newest) > 0 do %>
     <table border="0" cellpadding="0" cellspacing="0" class="itemlist">
-
-        <%= for {id, score, title, comments, url, updated} <- @top_newest do %>
-          <tr class="athing <%= class_update_animation(updated) %>" >
-          <td align="right" valign="top" class="title"><span class="rank">1.</span></td>
-            <td><%= score %></td>
-            <td><a href="<%= url %>"> <%= title %></a> </td>
-            <td><a href="https://news.ycombinator.com/item?id=<%= id %>"><%= comments %></a></td>
-          </tr>
-          <tr class="spacer" style="height:5px"></tr>
-        <% end %>
+      <%= for {{id, score, title, comments, url, updated},idx} <- Enum.with_index(@top_newest) do %>
+      <tr class="athing <%= class_update_animation(updated) %>" >
+        <td align="right" valign="top" class="title"><span class="rank"><%= idx + 1 %>.</span></td>
+        <td class="votelinks" valign="top">
+        <div class="votearrow" title="upvote"></div>
+        </td>
+        <td class="title">
+          <a class="storylink" href="<%= url %>"><%= title %></a>
+        </td>
+      </tr>
+      <tr class="<%= class_update_animation(updated) %>">
+        <td colspan="2"></td>
+        <td class="subtext">
+          <span class="score"><%= score %> points</span>
+          |
+          <a href="https://news.ycombinator.com/item?id=<%= id %>"><%= comments %> comments</a>
+        </td>
+      </tr>
+      <tr class="spacer" style="height:5px"></tr>
+      <% end %>
       </tbody>
     </table>
     <% else %>
-      <p> No data available yet. </p>
+    <p> No data available yet. </p>
     <% end %>
     </td>
     </tr>
+    <tr>
+    <td bgcolor="#ff6600">
+      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+          <td style="line-height:2px; height:2px;">
+          </td>
+        </tr>
+      </table>
+    </td>
+    </tr>
+    </tbody>
     </table>
     </center>
     """
