2d4ad64a646d478159d6967b482b1e5e261e1006
Gregor Stipicic <gregor.stipicic@gmail.com>
2020-05-31 17:53:01 +0200

    reporting sentiment score

--- lib/hnlive/sentiment.ex
+++ lib/hnlive/sentiment.ex
@@ -4,20 +4,20 @@ defmodule HNLive.Sentiment do
     word_list = Map.keys(valence_map)
     sample = File.read!("samples.txt")
 
-    {positive, neutral, negative} =
+    {positive, neutral, negative, score} =
       :binary.matches(sample, word_list)
       |> Enum.filter(fn {start, length} -> is_word?(sample, start, length) end)
       |> Enum.map(fn {start, length} -> binary_part(sample, start, length) end)
       |> Enum.map(fn word -> {word, Map.fetch!(valence_map, word)} end)
-      |> Enum.reduce({[], [], []}, fn {word, score}, {positive, neutral, negative} ->
+      |> Enum.reduce({[], [], [], 0}, fn {word, score}, {positive, neutral, negative, sum} ->
         cond do
-          score > 0 -> {[{word, score} | positive], neutral, negative}
-          score == 0 -> {positive, [{word, score} | neutral], negative}
-          score < 0 -> {positive, neutral, [{word, score} | negative]}
+          score > 0 -> {[{word, score} | positive], neutral, negative, sum + score}
+          score == 0 -> {positive, [{word, score} | neutral], negative, sum}
+          score < 0 -> {positive, neutral, [{word, score} | negative], sum + score}
         end
       end)
 
-    %{positive: positive, neutral: neutral, negative: negative}
+    %{positive: positive, neutral: neutral, negative: negative, score: score}
   end
 
   defp is_word?(binary, start, length) do
